5f7a8523ee983c10d486384443ac04cb60ca9ce4
Jane Doe <jane.doe@example.com>
2025-02-28 08:53:49 +0600

    new-commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 // app/not-found.tsx
 export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center h-screen text-center p-4">
             <h1 className="text-6xl font-bold text-gray-800">404</h1>
             <p className="text-lg text-gray-500 mt-2">Oops! The category page you're looking for doesn&apos;t exist.</p>
-            <a
+            <Link
                 href="/"
                 className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
             >
                 Go Back Home
-            </a>
+            </Link>
         </div>
     );
 }
